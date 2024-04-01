a04b780fd48bae881bdf666fb0cc87b724f33247
cjze <140057147+cjze@users.noreply.github.com>
2024-04-01 23:08:49 +0800

    change api key

--- lib/src/config/constant/app_constant.dart
+++ lib/src/config/constant/app_constant.dart
@@ -1,7 +1,7 @@
 class AppStrings {
   const AppStrings._();
 
-  static const apiKey = 'API_KEY';
+  static const apiKey = 'API_KEY ';
   static const resultJson =
       '{"coord":{"lon":-0.1257,"lat":51.5085},"weather":[{"id":721,"main":"Haze","description":"haze","icon":"50d"}],"base":"stations","main":{"temp":8.18,"feels_like":5.26,"temp_min":6.2,"temp_max":9.06,"pressure":994,"humidity":91},"visibility":5000,"wind":{"speed":5.14,"deg":70},"clouds":{"all":75},"dt":1711869316,"sys":{"type":2,"id":2075535,"country":"GB","sunrise":1711863432,"sunset":1711909897},"timezone":3600,"id":2643743,"name":"London","cod":200}';
 
